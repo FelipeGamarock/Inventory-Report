16e4015235463579aca9fca57298708fa7b34a6e
Felipe Gama Danta <epilefgama2@gmail.com>
2022-10-28 15:52:25 -0300

    feat: requisito 2, test class Product return

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -8,7 +8,7 @@ def test_cria_produto():
         "Golaço",
         "18/10/2022",
         "18/10/2024",
-        123456,
+        "123456",
         "não tem",
     )
     assert produto.id == 1
@@ -16,5 +16,5 @@ def test_cria_produto():
     assert produto.nome_da_empresa == "Golaço"
     assert produto.data_de_fabricacao == "18/10/2022"
     assert produto.data_de_validade == "18/10/2024"
-    assert produto.numero_de_serie == 123456
+    assert produto.numero_de_serie == "123456"
     assert produto.instrucoes_de_armazenamento == "não tem"
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "bola",
+        "Golaço",
+        "18/10/2022",
+        "18/10/2024",
+        "123456",
+        "não tem",
+    )
+    assert (
+        str(produto)
+        == "O produto bola fabricado em 18/10/2022 por Golaço "
+        + "com validade até 18/10/2024 precisa ser armazenado não tem."
+    )
