2eb59d1baeb2b53531a08ca9f27a3c3be834858c
Felipe Gama Danta <epilefgama2@gmail.com>
2022-10-28 15:33:18 -0300

    feat: requisito 1, test class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
-# iniciando projeto
+from inventory_report.inventory.product import Product
+
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "bola",
+        "Golaço",
+        "18/10/2022",
+        "18/10/2024",
+        123456,
+        "não tem",
+    )
+    assert produto.id == 1
+    assert produto.nome_do_produto == "bola"
+    assert produto.nome_da_empresa == "Golaço"
+    assert produto.data_de_fabricacao == "18/10/2022"
+    assert produto.data_de_validade == "18/10/2024"
+    assert produto.numero_de_serie == 123456
+    assert produto.instrucoes_de_armazenamento == "não tem"
