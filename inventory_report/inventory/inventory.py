da7b86cf229b9af9587c756a262866b57d483a1c
Felipe Gama Danta <epilefgama2@gmail.com>
2022-11-06 15:41:40 -0300

    feat: req 5, report from csv file

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,26 @@
+import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+
+    @classmethod
+    def import_data(cls, path, type):
+        if type == 'simples':
+            with open(path, encoding="utf-8") as file:
+                product_reader = csv.DictReader(file)
+                print(product_reader)
+                product_list = [product for product in product_reader]
+                print(product_list)
+                return SimpleReport.generate(product_list)
+        elif type == 'completo':
+            with open(path, encoding="utf-8") as file:
+                product_reader = csv.DictReader(file)
+                print(product_reader)
+                product_list = [product for product in product_reader]
+                print(product_list)
+                return CompleteReport.generate(product_list)
+
+
+Inventory.import_data("inventory_report/data/inventory.csv", 'simples')
