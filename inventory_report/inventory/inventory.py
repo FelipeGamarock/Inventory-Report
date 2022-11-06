6632434f6a3c73bfc13384b54f40e78fec00641d
Felipe Gama Danta <epilefgama2@gmail.com>
2022-11-06 17:17:36 -0300

    feat: req 6, report from json file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -7,20 +8,33 @@ class Inventory:
 
     @classmethod
     def import_data(cls, path, type):
-        if type == 'simples':
-            with open(path, encoding="utf-8") as file:
-                product_reader = csv.DictReader(file)
-                print(product_reader)
-                product_list = [product for product in product_reader]
-                print(product_list)
-                return SimpleReport.generate(product_list)
-        elif type == 'completo':
-            with open(path, encoding="utf-8") as file:
-                product_reader = csv.DictReader(file)
-                print(product_reader)
-                product_list = [product for product in product_reader]
-                print(product_list)
-                return CompleteReport.generate(product_list)
+        if "csv" in path:
+            if type == 'simples':
+                with open(path, encoding="utf-8") as file:
+                    product_reader = csv.DictReader(file)
+                    print(product_reader)
+                    product_list = [product for product in product_reader]
+                    print(product_list)
+                    return SimpleReport.generate(product_list)
+            elif type == 'completo':
+                with open(path, encoding="utf-8") as file:
+                    product_reader = csv.DictReader(file)
+                    print(product_reader)
+                    product_list = [product for product in product_reader]
+                    print(product_list)
+                    return CompleteReport.generate(product_list)
+
+        elif "json" in path:
+            if type == 'simples':
+                with open(path) as file:
+                    product_list = json.load(file)
+                    print(product_list)
+                    return SimpleReport.generate(product_list)
+            elif type == 'completo':
+                with open(path) as file:
+                    product_list = json.load(file)
+                    print(product_list)
+                    return CompleteReport.generate(product_list)
 
 
 Inventory.import_data("inventory_report/data/inventory.csv", 'simples')
